be95e4faf26a3ae29c74d149a1fcad75bf211475
aoe3 <aoe3@cornell.edu>
2018-02-28 20:12:58 -0500

    Update wwc.js

--- wwc/wwc.js
+++ wwc/wwc.js
@@ -290,25 +290,25 @@ svg2.append("g")
 
 /***************************BAD IDEA BELOW***************************/
 
-// var color = ["#faff16", "#d50527", "#158940", "#f898fd", "#24c9d7", "#cb9b64", 
-// 			"#866888", "#22e67a", "#e509ae", "#9dabfa", "#437e8a", "#b21bff", 
-// 			"#ff7b91", "#94aa05", "#ac5906", "#82a68d", "#fe6616", "#7a7352", 
-// 			"#f9bc0f", "#b65d66", "#07a2e6", "#c091ae", "#8a91a7", "#88fc07", 
-// 			"#ea42fe", "#9e8010", "#10b437", "#c281fe", "#f92b75", "#07c99d", 
-// 			"#a946aa", "#bfd544", "#16977e", "#ff6ac8", "#a88178", "#5776a9", 
-// 			"#678007", "#fa9316", "#85c070", "#6aa2a9", "#989e5d", "#fe9169", 
-// 			"#cd714a", "#6ed014", "#c5639c", "#c23271", "#698ffc", "#678275", 
-// 			"#c5a121", "#a978ba", "#ee534e", "#d24506", "#59c3fa", "#ca7b0a", 
-// 			"#6f7385", "#9a634a", "#48aa6f", "#ad9ad0", "#d7908c", "#6a8a53", 
-// 			"#8c46fc", "#8f5ab8", "#fd1105", "#7ea7cf", "#d77cd1", "#a9804b", 
-// 			"#0688b4", "#6a9f3e", "#ee8fba", "#a67389", "#9e8cfe", "#bd443c", 
-// 			"#6d63ff", "#d110d5", "#798cc3", "#df5f83", "#b1b853", "#bb59d8", 
-// 			"#1d960c", "#867ba8", "#18acc9", "#25b3a7", "#f3db1d", "#938c6d", 
-// 			"#936a24", "#a964fb", "#92e460", "#a05787", "#9c87a0", "#20c773", 
-// 			"#8b696d", "#78762d", "#e154c6", "#40835f", "#d73656", "#1afd5c", 
-// 			"#c4f546", "#3d88d8", "#bd3896", "#1397a3", "#f940a5", "#66aeff", 
-// 			"#d097e7", "#fe6ef9", "#d86507", "#8b900a", "#d47270", "#e8ac48", 
-// 			"#cf7c97", "#cebb11", "#718a90", "#e78139", "#ff7463", "#bea1fd"]
+var color = ["#bea1fd", "#d50527", "#158940", "#f898fd", "#24c9d7", "#cb9b64", 
+			"#866888", "#22e67a", "#e509ae", "#9dabfa", "#437e8a", "#b21bff", 
+			"#ff7b91", "#94aa05", "#ac5906", "#82a68d", "#fe6616", "#7a7352", 
+			"#f9bc0f", "#b65d66", "#07a2e6", "#c091ae", "#8a91a7", "#88fc07", 
+			"#ea42fe", "#9e8010", "#10b437", "#c281fe", "#f92b75", "#07c99d", 
+			"#a946aa", "#bfd544", "#16977e", "#ff6ac8", "#a88178", "#5776a9", 
+			"#678007", "#fa9316", "#85c070", "#6aa2a9", "#989e5d", "#fe9169", 
+			"#cd714a", "#6ed014", "#c5639c", "#c23271", "#698ffc", "#678275", 
+			"#c5a121", "#a978ba", "#ee534e", "#d24506", "#59c3fa", "#ca7b0a", 
+			"#6f7385", "#9a634a", "#48aa6f", "#ad9ad0", "#d7908c", "#6a8a53", 
+			"#8c46fc", "#8f5ab8", "#fd1105", "#7ea7cf", "#d77cd1", "#a9804b", 
+			"#0688b4", "#6a9f3e", "#ee8fba", "#a67389", "#9e8cfe", "#bd443c", 
+			"#6d63ff", "#d110d5", "#798cc3", "#df5f83", "#b1b853", "#bb59d8", 
+			"#1d960c", "#867ba8", "#18acc9", "#25b3a7", "#f3db1d", "#938c6d", 
+			"#936a24", "#a964fb", "#92e460", "#a05787", "#9c87a0", "#20c773", 
+			"#8b696d", "#78762d", "#e154c6", "#40835f", "#d73656", "#1afd5c", 
+			"#c4f546", "#3d88d8", "#bd3896", "#1397a3", "#f940a5", "#66aeff", 
+			"#d097e7", "#fe6ef9", "#d86507", "#8b900a", "#d47270", "#e8ac48", 
+			"#cf7c97", "#cebb11", "#718a90", "#e78139", "#ff7463", "#faff16"]
 
 /***************************BAD IDEA ABOVE***************************/
 
@@ -352,37 +352,38 @@ console.log(toPlot.length);
 for(w=0; w<toPlot.length;w++){
 	svg2.append("path")
 	    .attr("class", "thegraph")
-	    .attr("id", seenCountry[w])
-	    .style("stroke", "rgba(2,2,2,0.3)")
+	    .attr("id", seenCountry[w]+" "+color[w%color.length])
+	    .style("stroke", color[w%color.length])
+	    .attr("id2", color[w%color.length])
 	    .style("stroke-width", '3px')
 	    .style("fill", "none")
 		.attr("transform", "translate(0,"+(-margin.bottom)+")")
 	    .attr("d",d3Line(toPlot[w]))
 	    .on("mouseover", function (d) {
-	    	idString = "."+this.id;
+	    	var substring = this.id.split(" ");
+	    	idString = "."+substring[0];
 	    	d3.select(idString)
 	    		.style("font-weight", "bold")
-	    		.style("fill", "#4682b4") 
+	    		.style("fill", substring[1])
 				.style("font-size", "16px");
 
       		d3.select(this)                         
-	        	.style("stroke-width",'5px')
-	        	.style("stroke", "#4682b4");
+	        	.style("stroke-width",'5px');
         	
         	var selectthegraphs = $('.thegraph').not(this);    
         	d3.selectAll(selectthegraphs)
         		.style("opacity",0.2);
         })
         .on("mouseout",	function(d) { 
-	    	idString = "."+this.id;
+	    	var substring = this.id.split(" ");
+	    	idString = "."+substring[0];
 	    	d3.select(idString)
 	    		.style("font-weight", "normal")
-	    		.style("fill", "black") 
-			.style("font-size", "12px");  
+	    		.style("fill", "black")
+				.style("font-size", "12px");  
 
       		d3.select(this)
-        		.style("stroke-width",'3px')
-        		.style("stroke", "rgba(2,2,2,0.3)");
+        		.style("stroke-width",'3px');
         	
         	var selectthegraphs = $('.thegraph').not(this);
         	d3.selectAll(selectthegraphs)
@@ -422,3 +423,5 @@ for(aa=0; aa<loadedData.length; aa++){
 		.attr("text-anchor", "middle")
 		.text("WWC"+(aa+1));
 }
+
+
