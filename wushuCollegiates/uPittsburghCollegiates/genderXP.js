d975c00675e5c20727a4f509b39ec74efaeae920
aoe3 <aoe3@cornell.edu>
2018-03-03 08:57:57 -0500

    Update genderXP.js

--- wushuCollegiates/uPittsburghCollegiates/genderXP.js
+++ wushuCollegiates/uPittsburghCollegiates/genderXP.js
@@ -1,4 +1,4 @@
-$.getJSON("2016.json", function(data) {
+$.getJSON("2018.json", function(data) {
 	var genXPParagraph = document.getElementById("genderXPParagraph");
 
 	var gH = document.createElement("H3"); 
