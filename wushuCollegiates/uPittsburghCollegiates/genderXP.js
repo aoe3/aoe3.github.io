5fdafc4ef584f013037e29481872b6f0a4f99bf7
aoe3 <aoe3@cornell.edu>
2018-03-03 08:54:29 -0500

    Add files via upload

---
+++ wushuCollegiates/uPittsburghCollegiates/genderXP.js
@@ -0,0 +1,449 @@
+$.getJSON("2016.json", function(data) {
+	var genXPParagraph = document.getElementById("genderXPParagraph");
+
+	var gH = document.createElement("H3"); 
+	var gIntro = document.createTextNode("GENDER AND EXPERIENCE");
+	gH.appendChild(gIntro);
+	genXPParagraph.appendChild(gH);
+
+	var width = 375;
+	var height = 450;
+
+	var people = data;
+
+	var begcount = [0,0];
+	var intcount = [0,0];
+	var advcount = [0,0];
+
+	for (i = 0; i < people.length; i++){
+		var person = people[i]; 
+		if (person.gender == "F"){ 
+			if (person.experienceLevel == "BEG"){
+				begcount[0]++;
+				eventString = ""; 
+				for(j=0; j < person.events.length; j++){
+					if(j==(person.events.length -1)){
+						eventString = eventString + person.events[j];
+					} else {
+						eventString = eventString + person.events[j] + " | ";
+					}
+				}; 
+			}
+		}
+	};
+
+	for (i = 0; i < people.length; i++){
+		var person = people[i]; 
+		if (person.gender == "M"){ 
+			if (person.experienceLevel == "BEG"){
+				begcount[1]++;
+				eventString = ""; 
+				for(j=0; j < person.events.length; j++){
+					if(j==(person.events.length -1)){
+						eventString = eventString + person.events[j];
+					} else {
+						eventString = eventString + person.events[j] + " | ";
+					}
+				}; 
+			}
+		}
+	};
+
+	for (i = 0; i < people.length; i++){
+		var person = people[i]; 
+		if (person.gender == "F"){ 
+			if (person.experienceLevel == "INT"){
+				intcount[0]++;
+				eventString = ""; 
+				for(j=0; j < person.events.length; j++){
+					if(j==(person.events.length -1)){
+						eventString = eventString + person.events[j];
+					} else {
+						eventString = eventString + person.events[j] + " | ";
+					}
+				}; 
+			}
+		}
+	};
+
+	for (i = 0; i < people.length; i++){
+		var person = people[i]; 
+		if (person.gender == "M"){ 
+			if (person.experienceLevel == "INT"){
+				intcount[1]++;
+				eventString = ""; 
+				for(j=0; j < person.events.length; j++){
+					if(j==(person.events.length -1)){
+						eventString = eventString + person.events[j];
+					} else {
+						eventString = eventString + person.events[j] + " | ";
+					}
+				}; 
+			}
+		}
+	};
+
+	for (i = 0; i < people.length; i++){
+		var person = people[i]; 
+		if (person.gender == "F"){ 
+			if (person.experienceLevel == "ADV"){
+				advcount[0]++;
+				eventString = ""; 
+				for(j=0; j < person.events.length; j++){
+					if(j==(person.events.length -1)){
+						eventString = eventString + person.events[j];
+					} else {
+						eventString = eventString + person.events[j] + " | ";
+					}
+				}; 
+
+			}
+		}
+	};
+
+
+	for (i = 0; i < people.length; i++){
+		var person = people[i]; 
+		if (person.gender == "M"){ 
+			if (person.experienceLevel == "ADV"){
+				advcount[1]++;
+				eventString = ""; 
+				for(j=0; j < person.events.length; j++){
+					if(j==(person.events.length -1)){
+						eventString = eventString + person.events[j];
+					} else {
+						eventString = eventString + person.events[j] + " | ";
+					}
+				}; 
+			}
+		}
+	};
+
+	var svg = d3.select("#genderXPParagraph").append("svg")
+		.attr("width", width-100)
+		.attr("height", height)
+		.attr("id","svg1");
+	
+	svg.append("path")
+		.attr('d','M128.95011, 46.58577 L128.95011, 0.04056 A46.54521, 46.54521 1 1, 0 128.95011, 93.13098z')
+		.style('fill','#CDB87D')
+		.style('stroke','#1C2957')
+		.style('stroke-width', '2.5px')
+		.attr("transform", "translate(50,0), scale(0.65)")
+		.on("mouseover", function(){return tooltipM.style("visibility", "visible");})
+		.on("mousemove", function(){return tooltipM.style("top", (d3.event.pageY-5)+"px").style("left",(d3.event.pageX+5)+"px");})
+		.on("mouseout", function(){return tooltipM.style("visibility", "hidden");});
+
+	svg.append("path")
+		.attr('d','M128.80188, 388.15534 V549.98933 C124.26193, 579.39154, 80.494976, 579.39154, 80.108304, 549.98933 V388.15534 H22.821751 L84.404788, 171.89962 H74.379644 L38.57554, 295.0651 C29.524278, 322.30474, -7.3109962, 311.27714, 1.3392857, 282.17574 L41.43986, 148.98509 C46.051436, 133.68965, 65.32836, 106.68061, 98.726432, 106.02037 H128.80188z')
+		.style('fill','#CDB87D')
+		.style('stroke','#1C2957')
+		.style('stroke-width', '2.5px')
+		.attr("transform", "translate(50,0), scale(0.65)")
+		.on("mouseover", function(){return tooltipM.style("visibility", "visible");})
+		.on("mousemove", function(){return tooltipM.style("top", (d3.event.pageY-5)+"px").style("left",(d3.event.pageX+5)+"px");})
+		.on("mouseout", function(){return tooltipM.style("visibility", "hidden");});
+
+	svg.append("path")
+		.attr('d','M515.90545, 46.58577 L515.90545, 0.04056 A46.54521, 46.54521 1 0, 1 515.90545, 93.13098z')
+		.style('fill','#1C2957')
+		.style('stroke',"#CDB87D")
+		.style('stroke-width', '2.5px')
+        .attr("transform", "translate(-195,0), scale(0.65)")
+		.on("mouseover", function(){return tooltipF.style("visibility", "visible");})
+		.on("mousemove", function(){return tooltipF.style("top", (d3.event.pageY-5)+"px").style("left",(d3.event.pageX+5)+"px");})
+		.on("mouseout", function(){return tooltipF.style("visibility", "hidden");});
+
+	svg.append("path")
+		.attr('d','M515.48597, 106.02037 V330.86906 H515.48597 V549.82853 C516.18773, 579.72094, 570.13733, 580.80938, 569.90817, 542.82853 V179.06038 H579.93333 V310.81886 C579.46073, 339.01802, 619.54701, 339.01802, 620.03389, 310.81886 V167.60313 C619.46109, 137.00793, 595.67281, 106.56889, 559.88305, 106.02037 H515.49589z')
+		.style('fill','#1C2957')
+		.style('stroke',"#CDB87D")
+		.style('stroke-width', '2.5px')
+	    .attr("transform", "translate(-195,0), scale(0.65)")
+		.on("mouseover", function(){return tooltipF.style("visibility", "visible");})
+		.on("mousemove", function(){return tooltipF.style("top", (d3.event.pageY-5)+"px").style("left",(d3.event.pageX+5)+"px");})
+		.on("mouseout", function(){return tooltipF.style("visibility", "hidden");});
+
+	svg.append("text")
+		.attr("x", 137)
+		.attr("y", 400)
+		.text("Hover over me!")
+		.attr("text-anchor", "middle")
+		.style("alignment-baseline", "middle")
+		.style("fill", "white");
+
+	// console.log(begcount[0] + intcount[0] + advcount[0] + begcount[1] + intcount[1] + advcount[1]);
+
+	var tooltipM = d3.select("#genderXPParagraph")
+	    .append("div")
+	    .style("position", "absolute")
+	    .style("z-index", "10")
+	    .style("visibility", "hidden")
+	    .style("font-size", "12px")
+	    .style("text-align", "center")
+	    .html("There are " + begcount[0] + " people in <br>" + "the female beginner divisions,<br>" + intcount[0] + " in female intermediate,<br>" + "and " + advcount[0] + " in female advanced!");
+
+    var tooltipF = d3.select("#genderXPParagraph")
+	    .append("div")
+	    .style("position", "absolute")
+	    .style("z-index", "10")
+	    .style("visibility", "hidden")
+	    .style("font-size", "12px")
+	    .style("text-align", "center")
+	    .html("There are " + begcount[1] + " people in <br>" + "the male beginner divisions,<br>" + intcount[1] + " in male intermediate,<br>" + "and " + advcount[1] + " in male advanced!");
+
+
+	var svg2 = d3.select("#genderXPParagraph").append("svg")
+		.attr("width", width*2)
+		.attr("height", height)
+		.attr("id", "svg2");
+
+	var seen = [];
+	var instances = [];
+	for(g = 0; g < people.length; g++){
+		person = people[g];
+		sName = person.school;
+		if (seen.indexOf(sName) == -1){
+			seen.push(sName);
+			instances.push([person.experienceLevel]);
+		} else {
+			var ind = seen.indexOf(sName);
+			instances[ind].push(person.experienceLevel);
+		}
+	}
+
+	var margin = {top: 30, right: 40, bottom: 30, left: 100};
+    var newHeight = height - margin.top - margin.bottom/2 -1;
+
+    var xScale = d3.scale.linear()
+    	.domain([0, 1])
+    	.range([0,675]);
+
+	var yScale = d3.scale.linear()
+        .domain([seen.length,0])
+        .range([0,400]);
+
+    var formatPercent = d3.format(".0%");
+
+    var xAxis = d3.svg.axis()
+    	.orient("bottom")
+    	.tickFormat(formatPercent)
+    	.ticks(10)
+    	.scale(xScale);
+
+    var yAxis = d3.svg.axis()
+    	.orient("left")
+    	.tickFormat("")
+    	.scale(yScale);
+
+    svg2.append("g")
+        .attr("transform", "translate(" + margin.right + " ,"+newHeight+")")
+    	.call(xAxis)
+    	.style("fill", "#1C2957");
+    svg2.append("g")
+        .attr("transform", "translate(" + margin.right + " ,10)")
+    	.call(yAxis)
+    	.style("fill", "#1C2957");
+
+    
+
+    //#a4b2dc #7d92cd #3e4966
+    var counter = [];
+    var looper = 0;
+    var instancesNumberClassifier = [];
+    var instancesSumClassifier = [];
+    var instancesAggregateClassifier = [];
+    for(r=0; r < instances.length; r++){
+    	var nuInstance = [];
+    	var sum = 0;
+    	var agg = [0,0,0]
+	    for(s=0; s<(instances[r].length); s++){
+	    	if(instances[r][s] == "BEG"){
+	    		nuInstance.push(0);
+	    		sum++;
+	    		agg[0]++;
+	    	} else if (instances[r][s] == "INT"){
+	    		nuInstance.push(1);
+	    		sum++;
+	    		agg[1]++;
+	    	} else {
+	    		nuInstance.push(2);
+	    		sum++;
+	    		agg[2]++;
+	    	}
+	    }
+	    instancesSumClassifier.push(sum);
+	    instancesAggregateClassifier.push(agg);
+	    instancesNumberClassifier.push(nuInstance);
+    }
+/**for testing stuff*/
+ //    for(t = 0; t < instancesNumberClassifier.length; t++){
+	// 	instancesNumberClassifier[t].sort();
+	// }
+
+ //    for(u = 0; u < instancesNumberClassifier.length; u++){
+	// 	console.log("[" + seen[u] + " : {classes: " + instancesNumberClassifier[u] + ", total: "+instancesSumClassifier[u]+", brkdown: "+instancesAggregateClassifier[u]+"}]");
+	// }
+
+	for(v = 0; v<instances.length; v++){
+		var strng = seen[v];
+		var fraction = 1/instancesSumClassifier[v];
+		var arrayXPLevel = instancesAggregateClassifier[v];
+		//no beginners
+		if(arrayXPLevel[0] == 0){
+			//no beginners or int; only advanced
+			if(arrayXPLevel[1] == 0){
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(1))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#3e4966");
+			//only int
+			} else if(arrayXPLevel[2] == 0){
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(1))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#7d92cd");
+			//at least one int and adv
+			} else {
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[1]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#7d92cd");
+				
+				svg2.append("rect")
+					.attr("x",10 + xScale(fraction * arrayXPLevel[1])+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[2]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#3e4966");
+			}
+
+		//no int, but at least one beginner
+		} else if (arrayXPLevel[1] == 0){
+			//only beginner
+			if(arrayXPLevel[2] == 0){
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(1))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#a4b2dc");
+			//beginner and adv
+			} else {
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[0]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#a4b2dc");
+				svg2.append("rect")
+					.attr("x",10 + xScale(fraction * arrayXPLevel[0])+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[2]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#3e4966");
+			}
+		//at least one beginner, int
+		} else {
+			//no adv
+			if(arrayXPLevel[2] == 0){
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[0]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#a4b2dc");
+
+				svg2.append("rect")
+					.attr("x",10 + xScale(fraction * arrayXPLevel[0])+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[1]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#7d92cd");
+			} else {
+				svg2.append("rect")
+					.attr("x",10 + xScale(0)+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[0]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#a4b2dc");
+
+				svg2.append("rect")
+					.attr("x",10 + xScale(fraction * arrayXPLevel[0])+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[1]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#7d92cd");
+
+				svg2.append("rect")
+					.attr("x",10 + xScale(fraction * arrayXPLevel[0]) + xScale(fraction * arrayXPLevel[1])+margin.top)
+					.attr("y",yScale(v)-(height/instances.length)+5)
+					.attr("width",xScale(fraction * arrayXPLevel[2]))
+					.attr("height",height/instances.length)
+			        .attr("stroke", "#1c2957")
+					// .attr("transform","translate("+200+","+200+")")
+					.style("fill", "#3e4966");
+			}
+		}
+	}
+
+	for(r=0; r<9; r++){
+		svg2.append("line")
+			.attr("x1", xScale((r+1)/10))
+			.attr("y1", yScale(0))
+			.attr("x2", xScale((r+1)/10))
+			.attr("y2", yScale(seen.length))
+			.style("stroke", "black")
+			.style("opacity", 0.2)
+        	.style("stroke-dasharray", ("3, 3"))
+        	.attr("transform", "translate(" + margin.right + " ,5)");
+	}
+
+	for(q = 0; q<seen.length; q++){
+    	svg2.append("text")
+    		.attr("x",xScale(1)+margin.top)
+    		.attr("y",yScale(q)+2)
+    		.text(seen[q])
+    		.attr("font-size", "12px")
+    		.attr("text-anchor", "end")
+    		.style("fill", "white");
+    }
+
+    svg2.append("text")
+    	.attr("x", width)
+    	.attr("y", height-3)
+    	.attr("text-anchor", "middle")
+    	.style("font-size", "15px")
+    	.text("The lightest color is beginner. The darkest color is advanced.")
+    	.style("fill", "#1C2957");
+
+
+});
---
+++ wushuCollegiates/uPittsburghCollegiates/locations.js
@@ -0,0 +1,220 @@
+$.getJSON("2016.json", function(data) {
+	var people = data;
+	var localPara = document.getElementById("locationsParagraph");
+
+	var lH = document.createElement("H3"); 
+	var lIntro = document.createTextNode("COMPETITOR LOCATIONS");
+	lH.appendChild(lIntro);
+	localPara.appendChild(lH);
+
+	var arcdata=[];
+	var circleData=[];
+
+	$.getJSON("locationCoords.json", function(data) {
+		var places = data;
+		for(i = 0; i < places.length; i++){
+			circleData.push(places[i].coords);
+
+			for(f=0; f < people.length; f++){
+				var pName = people[f].school;
+				
+				if((places[i].name == pName) && (places[i].name != "U Pittsburgh")){
+					var startCoords = places[i].coords;
+					var targetCoords = places[0].coords;
+					var amt = schoolsList[places[i].name];
+					var newArc = {sourceLocation: startCoords, targetLocation: targetCoords, amount: amt};
+					arcdata.push(newArc);
+				}
+			}
+		}
+	});
+
+//***********Map drawing code adapted from bl.ocks.org
+
+	// Map dimensions (in pixels)
+	var width = 500,
+			height = 750;
+
+	// Map projection
+	var projection = d3.geo.albersUsa()
+			.scale(730.1630554896399)
+			.translate([width/2,height/2]) //translate to center the map in view
+
+	// Generate paths based on projection
+	var path = d3.geo.path()
+			.projection(projection);
+
+	// Create an SVG
+	var svg = d3.select("#locationsParagraph").append("svg")
+			.attr("width", width)
+			.attr("height", height);
+
+	// Group for the states
+	// SVG drawing order is based strictly on the order in the DOM
+	// So you can't use something like z-index to make an element appear above or below another object
+	// We have to draw the states group first so that it appears below the arcs
+	// Change the order of these two variables if you want to see how it would look incorrect.
+	var states = svg.append("g")
+			.attr("class","states");
+
+	// Group for the arcs
+	var arcs = svg.append("g")
+			.attr("class","arcs");
+
+	// Group for circles
+	var circles = svg.append("g")
+			.attr("class","circles");
+
+	// Keeps track of currently zoomed feature
+	var centered;
+
+	// Load the basemap data
+	d3.json("us.json",function(error,geodata) {
+		if (error) return console.log(error); //unknown error, check the console
+
+		//Create a path for each map feature in the data
+		states.selectAll("path")
+			.data(topojson.feature(geodata,geodata.objects.states).features) //generate features from TopoJSON
+			.enter()
+			.append("path")
+			.attr("d",path)
+
+		//Draw circles.
+		circles.selectAll("path")
+			.data(circleData)
+			.enter()
+			.append("circle")
+		    .attr("transform", function(d) { 
+		        return "translate(" + projection(d) + ")"; 
+		    })
+		    .attr('r', 2.5)
+		    .style("fill", "white")
+		    .attr("opacity", 0.9);
+
+		// Create a path for each source/target pair.
+		arcs.selectAll("path")
+			.data(arcdata)
+			.enter()
+			.append("path")
+			.attr('d', function(d) { 
+				return lngLatToArc(d, 'sourceLocation', 'targetLocation', 3); // A bend of 5 looks nice and subtle, but this will depend on the length of your arcs and the visual look your visualization requires. Higher number equals less bend.
+			})
+			.style("stroke-width", function(d){return 'amount' * 5});
+
+	});
+
+	// This function takes an object, the key names where it will find an array of lng/lat pairs, e.g. `[-74, 40]`
+	// And a bend parameter for how much bend you want in your arcs, the higher the number, the less bend.
+	function lngLatToArc(d, sourceName, targetName, bend){
+		// If no bend is supplied, then do the plain square root
+		bend = bend || 1;
+		// `d[sourceName]` and `d[targetname]` are arrays of `[lng, lat]`
+		// Note, people often put these in lat then lng, but mathematically we want x then y which is `lng,lat`
+
+		var sourceLngLat = d[sourceName],
+				targetLngLat = d[targetName];
+
+		if (targetLngLat && sourceLngLat) {
+			var sourceXY = projection( sourceLngLat ),
+					targetXY = projection( targetLngLat );
+
+			// Uncomment this for testing, useful to see if you have any null lng/lat values
+			// if (!targetXY) console.log(d, targetLngLat, targetXY)
+			var sourceX = sourceXY[0],
+					sourceY = sourceXY[1];
+
+			var targetX = targetXY[0],
+					targetY = targetXY[1];
+
+			var dx = targetX - sourceX,
+					dy = targetY - sourceY,
+					dr = Math.sqrt(dx * dx + dy * dy)*bend;
+
+			// To avoid a whirlpool effect, make the bend direction consistent regardless of whether the source is east or west of the target
+			var west_of_source = (targetX - sourceX) < 0;
+			if (west_of_source) return "M" + targetX + "," + targetY + "A" + dr + "," + dr + " 0 0,1 " + sourceX + "," + sourceY;
+			return "M" + sourceX + "," + sourceY + "A" + dr + "," + dr + " 0 0,1 " + targetX + "," + targetY;
+			
+		} else {
+			return "M0,0,l0,0z";
+		}
+			// Zoom to feature on click
+		// This is optional but if you use mapstarter.com, you get it for free.
+		function clicked(d,i) {
+			//Add any other onClick events here
+			var x, y, k;
+			if (d && centered !== d) {
+		    // Compute the new map center and scale to zoom to
+				var centroid = path.centroid(d);
+				var b = path.bounds(d);
+				x = centroid[0];
+				y = centroid[1];
+				k = .8 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+				centered = d
+			} else {
+				x = width / 2;
+				y = height / 2;
+				k = 1;
+				centered = null;
+			}
+			// Highlight the new feature
+			states.selectAll("path")
+				.classed("highlighted",function(d) {
+						return d === centered;
+				})
+				.style("stroke-width", 1 / k + "px"); // Keep the border width constant
+			//Zoom and re-center the whole map container
+			//Comment `.transition()` and `.duration()` to eliminate gradual zoom
+			svg
+				.transition()
+				.duration(500)
+				.attr("transform","translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+		}
+	}
+//*****************End adapted code.
+
+	
+
+	var breakLine = document.createElement('br');
+	localPara.appendChild(breakLine);
+
+	var seen = [];
+	var schoolsList = {};
+	for(g = 0; g < people.length; g++){
+		person = people[g];
+		if (seen.indexOf(person.school) == -1){
+			schoolsList[person.school] = 1;
+			seen.push(person.school);
+		} else {
+			schoolsList[person.school] +=1;
+		}
+	}
+
+	var schoolString = "";
+	for(h = 0; h < seen.length; h++){
+		if (h == (seen.length - 1)){
+			schoolString += "and " + seen[h] + "!";
+		} else {
+			schoolString = schoolString +  seen[h] + "; " ;
+		}
+	}
+
+
+	var eachSchoolText = document.createElement("P");
+	var keys = Object.keys(schoolsList).sort()
+	keys.forEach(function(key,index) {
+		var indivSchool = document.createTextNode(key + ": " + schoolsList[key] + " representative(s)! ");
+		eachSchoolText.appendChild(indivSchool);
+		var breakLine = document.createElement('br');
+		eachSchoolText.appendChild(breakLine);
+	});
+
+	localPara.append(eachSchoolText);
+
+	var breakLine = document.createElement('br');
+	localPara.appendChild(breakLine);
+	var breakLine = document.createElement('br');
+	localPara.appendChild(breakLine);
+	var breakLine = document.createElement('br');
+	localPara.appendChild(breakLine);
+});
